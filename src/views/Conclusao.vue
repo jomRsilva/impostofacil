5fcbb967a6339a9f5482d0383ccb9ef748d92ed1
jomRsilva <jv.dnc00@gmail.com>
2023-11-01 10:38:18 -0300

    page: Completion page w/ mobile

--- src/views/Conclusao.vue
+++ src/views/Conclusao.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="home">
-		conclusao
+  <div class="dflex conclusao">
+		<img src="../assets/img/irpflogo.svg" class="irpf_logo" alt="IRPF Logo">
+		<h1> Entrega realizada com sucesso! </h1>
+		<p>
+			Fique atento aos seus canais de comunicação informados (e-mail e telefone) <br>
+			para eventuais atualizações sobre suas declarações.
+		</p>
+		<router-link to="/" class="dflex btn btn_fill">
+			<small> Retornar ao painel principal </small>
+		</router-link>
   </div>
 </template>
 
